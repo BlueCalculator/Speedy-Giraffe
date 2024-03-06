b3d05f80a31c4ab7f263d681a4a8a9c60ee1b6d4
BlueCalculator <85308130+BlueCalculator@users.noreply.github.com>
2024-03-06 16:44:42 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 setInterval(time, 100)
 function time() {
     if (document.querySelector("div.ad-showing")) { 
-        //AD IS ACTIVE
-        document.querySelector('video').playbackRate = 16.0;
+            let vid = document.querySelector('video')
+            let skiplength = vid.duration
+            vid.currentTime = skiplength;
         }
     }
